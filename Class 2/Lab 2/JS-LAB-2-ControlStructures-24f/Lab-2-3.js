f6ad4cfa20fbcfbdc7c98a465456fdbe8e189acf
Jerad Beauregard <jeradbeauregard@gmail.com>
2024-09-16 14:08:07 -0400

    front end class 2 lab part 3

--- Class 2/Lab 2/JS-LAB-2-ControlStructures-24f/Lab-2-3.js
+++ Class 2/Lab 2/JS-LAB-2-ControlStructures-24f/Lab-2-3.js
@@ -1,7 +1,32 @@
 //######## LAB 2-3 EMAIL SIGNUP ########
-alert("hey 2.3");//COMMENT OUT ONCE CONNECTED TO YOUR HTML PAGE
+//alert("hey 2.3");//COMMENT OUT ONCE CONNECTED TO YOUR HTML PAGE
 //==== VARIABLES =========
-
+var userInput
+var emailAdress
 
 
 //==== LOGIC =============
+
+userInput = confirm("Would you like to join our email list?");
+console.log(userInput);
+
+if (userInput === true){
+ emailAdress = prompt("Please enter your email adress: ", "me@example.ca");
+    
+            if (emailAdress === "" || emailAdress === "me@example.ca" || emailAdress === null){
+                alert("Thank you, but your email was not valid.");
+            }
+            else{ 
+            alert("Thank you, our next newsletter will be sent to " + emailAdress);
+            }
+}
+
+else{
+    alert("Thank you, we will not bother you again.");
+}
+
+
+
+
+
+
